727eb9da2e0f6826816a382ffb859f667b7d1bde
Arif351 <ariful8836@gmail.com>
2022-11-10 12:53:07 +0600

    review

--- src/Components/Pages/DetailsService/DetailsService.js
+++ src/Components/Pages/DetailsService/DetailsService.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const DetailsService = () => {
+    const { customer } = useContext(AuthContext);
+
     const { title, coverImg, about, details, price, rating, _id, review_1, review_2 } = useLoaderData();
+
     return (
         <div data-theme="dracula" className="card card-compact w-2/4 bg-base-100 shadow-xl mx-auto my-12">
             <figure><img src={coverImg} alt="" /></figure>
@@ -18,16 +22,43 @@ const DetailsService = () => {
                 <hr />
                 <div className="flex flex-col w-full">
                     <h1 className='text-center text-2xl my-3'>Customer's Reviews:</h1>
-                    <div className="grid h-48 card bg-base-300 rounded-box place-items-center">{review_1}</div>
+                    <div className="grid h-32 card bg-base-300 rounded-box place-items-center">{review_1}</div>
                     <div className="divider"></div>
-                    <div className="grid h-48 card bg-base-300 rounded-box place-items-center">{review_2}</div>
+                    <div className="grid h-32 card bg-base-300 rounded-box place-items-center">{review_2}</div>
                 </div>
+                <form>
+                    <div className="card-body">
+                        <h1 className="text-2xl font-bold text-center">Write Your Review</h1>
+                        <hr />
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' defaultValue={customer?.name} className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo Url</span>
+                            </label>
+                            <input type="text" name='url' defaultValue={customer?.url} className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' defaultValue={customer?.email} className="input input-bordered" required />
+                        </div>
+                    </div>
+                    {/* <input type="text" name='text' defaultValue={customer?.name} className="mb-2 input textarea-info w-6/12 max-w-xs" /> <br />
+                    <input type="email" name='email' defaultValue={customer?.email} className="mb-2 input textarea-info w-6/12 max-w-xs" /> */}
+                </form>
+                <textarea className="textarea textarea-info  my-4" placeholder="Write your Review"></textarea>
+
                 <div className="card-actions justify-around mt-3">
                     <button className="btn btn-outline">Request for Service</button>
                     <Link to='/login'><button className="btn btn-outline">Give us a review</button></Link>
                 </div>
-                <div>
-                </div>
+
             </div>
         </div >
     );
